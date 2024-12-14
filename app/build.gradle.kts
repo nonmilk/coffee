eabffdd363e772408276d287208c1e338d887557
shimeoki <shimeoki@gmail.com>
2024-12-14 11:00:06 +0300

    initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    // id("application")
+}
+
+group = "io.github.nonmilk"
+version = "0.1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+// application {
+//     mainClass = "..."
+// }
