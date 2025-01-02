3977e2b5d408bcfa94f1de5ebef4cead789f846e
shimeoki <shimeoki@gmail.com>
2025-01-02 20:02:23 +0300

    feat: add main controller and view

--- app/src/main/java/io/github/nonmilk/coffee/App.java
+++ app/src/main/java/io/github/nonmilk/coffee/App.java
@@ -3,6 +3,8 @@ package io.github.nonmilk.coffee;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public final class App extends Application {
@@ -12,7 +14,15 @@ public final class App extends Application {
 
     @Override
     public void start(final Stage stage) throws IOException {
-        stage.show(); // black screen
+        final FXMLLoader l = new FXMLLoader(
+                Grinder.class.getResource("grinder.fxml"));
+
+        final Scene scene = new Scene(l.load());
+
+        stage.setTitle("coffee-grinder");
+        stage.setScene(scene);
+
+        stage.show();
     }
 
     public static void main(final String[] args) {
