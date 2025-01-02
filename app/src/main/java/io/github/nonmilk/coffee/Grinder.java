01e27504885ada4ed68ab618eeca552b24cc8561
shimeoki <shimeoki@gmail.com>
2025-01-02 21:09:19 +0300

    feat: use Scener in Grinder

--- app/src/main/java/io/github/nonmilk/coffee/Grinder.java
+++ app/src/main/java/io/github/nonmilk/coffee/Grinder.java
@@ -1,5 +1,17 @@
 package io.github.nonmilk.coffee;
 
+import javafx.fxml.FXML;
+
 public final class Grinder {
 
+    @FXML
+    private Viewer viewController;
+
+    @FXML
+    private Scener sceneController;
+
+    @FXML
+    private void initialize() {
+        sceneController.setRenderer(viewController.renderer());
+    }
 }
