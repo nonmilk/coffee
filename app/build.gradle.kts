2514260d0b46c93bdd5733bf859e274e79fe8008
shimeoki <shimeoki@gmail.com>
2025-01-02 19:45:58 +0300

    build: add javafx

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("application")
+    id("org.openjfx.javafxplugin") version "0.1.0"
 }
 
 group = "io.github.nonmilk"
@@ -17,6 +18,11 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(21)
     }
+}
+
+javafx {
+    version = "21"
+    modules("javafx.controls", "javafx.fxml")
 }
 
 // application {
