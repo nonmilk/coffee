7c3acc699a80fae7d12db51f7ee9adb23515cccb
shimeoki <shimeoki@gmail.com>
2025-01-03 20:20:19 +0300

    feat(Camerer): implement logic

--- app/src/main/java/io/github/nonmilk/coffee/Camerer.java
+++ app/src/main/java/io/github/nonmilk/coffee/Camerer.java
@@ -1,5 +1,7 @@
 package io.github.nonmilk.coffee;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import io.github.nonmilk.coffee.grinder.camera.Camera;
@@ -12,14 +14,89 @@ import io.github.nonmilk.coffee.grinder.render.Scene;
 
 public final class Camerer {
 
-    // TODO multiple cameras
+    private static final String DEFAULT_NAME = "camera";
+    private int namePostfix = 1;
+
     private Scene scene;
 
-    // FIXME test code
+    private final Map<Scene, Map<String, NamedCamera>> scenes = new HashMap<>();
+    private final Map<Scene, NamedCamera> activeCameras = new HashMap<>();
+
+    private Map<String, NamedCamera> cameras = new HashMap<>();
+    private NamedCamera active;
+
     public void setScene(final Scene s) {
         scene = Objects.requireNonNull(s);
-        scene.cameras().add(camera());
-        scene.selectCamera(0);
+
+        final var cameras = scenes.get(scene);
+        if (cameras != null) {
+            this.cameras = cameras;
+            return;
+        }
+
+        this.cameras = new HashMap<>();
+        scenes.put(scene, this.cameras);
+
+        final String name = name();
+
+        final var camera = scene.camera();
+        if (camera != null) {
+            add(camera, name);
+        } else {
+            add(camera(), name); // FIXME
+        }
+
+        select(name);
+    }
+
+    private void add(final Camera c, final String name) {
+        if (cameras.get(name) != null) {
+            throw new IllegalArgumentException("this name already exists");
+        }
+
+        cameras.put(name, new NamedCamera(c, name));
+    }
+
+    private void remove(final String name) {
+        final var camera = cameras.get(name);
+
+        if (camera == null) {
+            throw new IllegalArgumentException("this name doesn't exist");
+        } else if (camera == active) {
+            throw new IllegalArgumentException("cannot remove active camera");
+        }
+
+        cameras.remove(name);
+    }
+
+    private void rename(final String oldName, final String newName) {
+        final var camera = cameras.get(oldName);
+        if (camera == null) {
+            throw new IllegalArgumentException("this name doesn't exist");
+        }
+
+        if (cameras.get(newName) != null) {
+            throw new IllegalArgumentException("this name already exists");
+        }
+
+        cameras.remove(oldName);
+        camera.rename(newName);
+        cameras.put(newName, camera);
+    }
+
+    private void select(final String name) {
+        final var camera = cameras.get(name);
+        if (camera == null) {
+            throw new IllegalArgumentException("this name doesn't exist");
+        }
+
+        active = camera;
+        activeCameras.put(scene, active);
+        scene.setCamera(active());
+    }
+
+    private Camera active() {
+        return active.unwrap();
     }
 
     // FIXME test code
@@ -28,5 +105,9 @@ public final class Camerer {
         final var view = new PerspectiveView((float) ((70f * Math.PI) / 100f), 1.5f);
         final var box = new ClippingBox(0.1f, 10);
         return new PerspectiveCamera(orientation, view, box);
+    }
+
+    private String name() {
+        return String.format("%s %d", DEFAULT_NAME, namePostfix++);
     }
 }
