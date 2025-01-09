f6d7c35e61da26dcbcc31db4fce042059df44d0e
shimeoki <shimeoki@gmail.com>
2025-01-09 18:34:31 +0300

    feat: add Selection

---
+++ app/src/main/java/io/github/nonmilk/coffee/Selection.java
@@ -0,0 +1,29 @@
+package io.github.nonmilk.coffee;
+
+public final class Selection {
+
+    private int x;
+    private int y;
+
+    private int width;
+    private int height;
+
+    public Selection() {
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    public int width() {
+        return width;
+    }
+
+    public int height() {
+        return height;
+    }
+}
