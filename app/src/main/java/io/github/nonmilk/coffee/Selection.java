93831e94bd4c47d70c736972ac63aea34c510efb
shimeoki <shimeoki@gmail.com>
2025-01-09 18:36:10 +0300

    feat(Selection): update()

--- app/src/main/java/io/github/nonmilk/coffee/Selection.java
+++ app/src/main/java/io/github/nonmilk/coffee/Selection.java
@@ -1,5 +1,7 @@
 package io.github.nonmilk.coffee;
 
+import io.github.shimeoki.jfx.rasterization.Point2i;
+
 public final class Selection {
 
     private int x;
@@ -25,5 +27,19 @@ public final class Selection {
 
     public int height() {
         return height;
+    }
+
+    public void update(final Point2i p1, final Point2i p2) {
+        final int x1 = p1.x();
+        final int x2 = p2.x();
+
+        final int y1 = p1.y();
+        final int y2 = p2.y();
+
+        x = Math.min(x1, x2);
+        width = Math.abs(x1 - x2);
+
+        y = Math.min(y1, y2);
+        height = Math.abs(y1 - y2);
     }
 }
