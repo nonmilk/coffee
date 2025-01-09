c8a59126816582a6ee3506717ba0734eb3fdc29a
shimeoki <shimeoki@gmail.com>
2025-01-09 17:16:45 +0300

    feat(CoffeeError): add other constructors

--- app/src/main/java/io/github/nonmilk/coffee/CoffeeError.java
+++ app/src/main/java/io/github/nonmilk/coffee/CoffeeError.java
@@ -5,4 +5,12 @@ public final class CoffeeError extends RuntimeException {
     public CoffeeError(final String msg, final Throwable e) {
         super(msg, e);
     }
+
+    public CoffeeError(final String msg) {
+        super(msg);
+    }
+
+    public CoffeeError(final Throwable e) {
+        super(e);
+    }
 }
