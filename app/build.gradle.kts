0c249e55687b95641826145fe9b3097447c06534
shimeoki <shimeoki@gmail.com>
2025-01-10 02:56:08 +0300

    build: add launch4j setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("application")
     id("org.openjfx.javafxplugin") version "0.1.0"
+    id("edu.sc.seis.launch4j") version "3.0.6"
 }
 
 group = "io.github.nonmilk"
@@ -26,6 +27,12 @@ java {
     }
 }
 
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "io.github.nonmilk.coffee.App"
+    }
+}
+
 javafx {
     version = "21"
     modules("javafx.controls", "javafx.fxml")
@@ -33,4 +40,10 @@ javafx {
 
 application {
     mainClass = "io.github.nonmilk.coffee.App"
+}
+
+launch4j {
+    mainClassName = "io.github.nonmilk.coffee.App"
+    icon = "${projectDir}/java.ico"
+    stayAlive = true
 }
